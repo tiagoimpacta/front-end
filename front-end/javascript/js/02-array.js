0d2b2e1fa0cb5b01b0f72a26be19f29b5b106d98
Tiago Bezerra dos Santos <97289582+tiagoimpacta@users.noreply.github.com>
2023-10-09 23:05:32 -0300

    JS: sintaxe, log, let/const, temp. string, array

---
+++ front-end/javascript/js/02-array.js
@@ -0,0 +1,26 @@
+/* Array: Vetores e/ou matrizes
+Lista/relação de dados indexados,
+separados por vírgula. */
+let alunos = ["Dio", "Ozzy", "Gillan"];
+
+console.log(
+`O ${alunos[1]} foi o primeiro vocalista do Black Sabbath.`
+);
+
+/* Atenção: índice SEMPRE inicia em
+zero 0. */
+
+
+/* Exercícios
+1) Crie uma constante com uma lista de linguagens: PHP, JavaScript, SQL, HTML5 e CSS3. */
+const linguagens = ["PHP", "JavaScript", "SQL", "HTML5", "CSS3"];
+
+/* 2) Faça uma saída de dados com a seguinte mensagem:
+
+"HTML5 e JavaScript são populares no Front-End, enquanto que PHP é bastante usada no Back-End."   */
+console.log(
+`${linguagens[3]} e ${linguagens[1]} são populares no Front-End, enquanto que ${linguagens[0]} é bastante usada no Back-End.`);
+
+console.log(
+linguagens[3]+" e "+linguagens[1]+" são populares no Front-End, enquanto que "+linguagens[0]+" é bastante usada no Back-End.");
+
