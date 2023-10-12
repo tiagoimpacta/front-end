0240582a2776d65167160f1fed3c0105b122447a
Tiago Bezerra dos Santos <97289582+tiagoimpacta@users.noreply.github.com>
2023-10-11 22:56:27 -0300

    JS: e/ou/não, loops, exercícios

--- front-end/javascript/js/04-condicionais.js
+++ front-end/javascript/js/04-condicionais.js
@@ -41,9 +41,79 @@ let resultado2;
 //     resultado2 = "Negativo";
 // }
 
-// Operador ternário ?:
+// Operador ternário ?: (shorthand if/else)
 resultado2 = numero >= 0 ? "Positivo" : "Negativo";
 console.log(numero, resultado2);
+
+/* Operadores Lógicos
+&&  ->  E (AND)
+||  ->  OU (OR)
+!   ->  NÃO/NEGAÇÃO (NOT) */
+
+// Banco de Dados fictício
+const loginCadastrado = "tiago.bsantos";
+const senhaCadastrada = "impacta123";
+
+// Dados de entrada
+let login = "tiago.bsantos";
+let senha = "impacta123";
+
+
+// OPERADOR &&
+if(login === loginCadastrado && senha === senhaCadastrada){
+    console.log("Ok! Entrada permitida...");
+} else {
+    console.log("Ops! Dados incorretos, verifique...");
+}
+
+/* Sobre duplo e triplo = */
+let a = "10"; // string
+let b = 10; // number
+
+// ==  considera SOMENTE valor
+console.log( a == b );  // true
+
+// === considera valor E tipo de dados
+console.log( a === b ); // false
+
+// OPERADOR ||
+let preco = 1000;
+let desconto = 200;
+let qtdEmEstoque = 50;
+
+/* Dar desconto no preço se quantidade em
+estoque for maior ou igual a 100 OU
+se o preço for maior que 3000 */
+if(qtdEmEstoque >= 100 || preco > 3000){
+    // preco = preco - desconto;
+    preco -= desconto;    
+    /* -=   operador subtrai e atribui */
+} 
+console.log(`Preço: ${preco}`);
+
+// Operador ! (NEGAÇÃO/NÃO)
+let blackFriday = false; // flag
+
+/* LÓGICA INVERSA/CONTRÁRIA
+Se NÃO estivermos no período de Black Friday,
+preços ficam normal. Caso contrário, preços com
+descontos. */
+if( !blackFriday ){
+    console.log("Fora da Black Friday, preços normais");
+} else {
+    console.log("Estamos na Black Friday, descontos!");
+}
+
+
+
+
+
+/* LÓGICA COMUM/HABITUAL
+Se estivermos no período de Black Friday,
+preços com descontos. Caso contrário, preços 
+ficam normal */
+
+
 
 
 
---
+++ front-end/javascript/js/05-loops.js
@@ -0,0 +1,81 @@
+"use strict";
+
+/* Estruturas de Repetição
+Instruções para realizar tarefas
+por uma quantidade específica de vezes.  
+
+Comandos tradicionais:
+- while     -> ENQUANTO
+- do/while  -> FAÇA/ENQUANTO
+- for       -> PARA 
+
+Obs.: normalmente o loop é controlado
+atráves de uma variável contadora.
+Nomes comuns para esta variável:
+i, j, k */
+
+// WHILE
+// let i = 1;
+// while( i <= 5 ){
+//     console.log(`Valor é: ${i}`);
+//     i++; // ++ incremento
+// }
+
+// DO/WHILE  FAÇA/ENQUANTO
+// let i = 1;
+// do {
+//     console.log(`Valor é: ${i}`);
+//     i++;
+// } while( i <= 5 )
+
+// FOR
+// for( let i = 1; i <= 10; i++ ){
+//     console.log(`Valor de i é ${i}`);
+// }
+
+
+// Loop com Array
+let alunos = [
+    "Ozzy", "Dio", "Gillan", "Martin", "Hughes", "Fagner"
+];
+
+// Guardando o tamanho do array na memória (cache)
+let quantidade = alunos.length;
+
+for( let i = 0; i < quantidade; i++ ) {
+    console.log(`Aluno: ${alunos[i]}`);
+} 
+
+/* Exercícios
+1) Crie um novo array chamado "clientes" contendo 3 objetos. Cada objeto deverá ter uma propriedade "nome" e uma propriedade "data de nascimento". Invente os nomes e as datas de nascimento. */
+let clientes = [
+    {
+        nome: "David Coverdale",
+        data: "20/10/1950"
+    },
+    {
+        nome: "Jon Oliva",
+        data: "15/09/1955"
+    },
+    {
+        nome: "Syd Barret",
+        data: "01/01/1945"
+    }
+];
+
+
+/* 2) Faça um loop (qualquer um dos que vimos) e mostre no console apenas o "nome" de cada cliente. Exemplo de saída:
+- Cliente: David Coverdale
+- Cliente: Jon Oliva
+- Cliente: Syd Barret */
+let qtdClientes = clientes.length;
+for( let i = 0; i < qtdClientes; i++ ){
+    console.log(`- Cliente: ${clientes[i].nome}`);
+}
+
+/* Loops exclusivo do JS */
+
+// for/of para arrays
+
+// for/in para objetos
+
