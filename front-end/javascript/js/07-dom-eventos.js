47b4fa9eab2e79bd92bb7b8df923db8eae878a68
Tiago Bezerra dos Santos <97289582+tiagoimpacta@users.noreply.github.com>
2023-10-17 23:14:14 -0300

    JS: DOM e Eventos

--- front-end/javascript/js/07-dom-eventos.js
+++ front-end/javascript/js/07-dom-eventos.js
@@ -25,3 +25,41 @@ console.log(descricaoDom);
 // Seleção usando querySelectorAll
 const exemplos = document.querySelectorAll(".exemplo");
 console.log(exemplos);
+
+
+/* Modificando elementos no DOM */
+exemploSubtitulo.innerHTML = "Brincando com JS!";
+exemploSubtitulo.style.background = "salmon";
+
+/* Trabalhando com eventos */
+const exemplo01 = document.querySelector("#exemplo01");
+const pagina = document.querySelector("body");
+const mensagem = document.querySelector("#mensagem");
+
+// Ouvinte de Evento
+exemplo01.addEventListener("click", function(){
+    pagina.style.fontFamily = "Verdana";
+    mensagem.innerHTML = "Clicou! :)";
+});
+
+// Modo Noturno
+// const botaoNoturno = document.querySelector("#noturno");
+// botaoNoturno.addEventListener("click", function(){
+//     pagina.classList.toggle("noturno");
+// });
+
+//Modo Noturno
+const botaoNoturno = document.querySelector("#noturno");
+
+botaoNoturno.addEventListener("click", function (){
+    pagina.classList.toggle("noturno");
+
+    /* Se a página está com a classe noturno */
+    if(pagina.className == "noturno"){
+        // Mudamos o texto do botão pra Desativar
+        botaoNoturno.textContent = "Desativar";
+    } else {
+        // Mudamos o texto do botão pra Ativar
+        botaoNoturno.textContent = "Ativar";
+    }
+});
